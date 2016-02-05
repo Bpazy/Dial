2bcad7db05243103d7911c5a90432ab6bcd49857
Bpazy <hanziyuan08@outlook.com>
2016-02-05 14:01:45 +0800

    优化通过短信获取密码的方法

--- app/src/main/java/bpazy/dial/MessageReceiverFromSms.java
+++ app/src/main/java/bpazy/dial/MessageReceiverFromSms.java
@@ -3,14 +3,15 @@ package bpazy.dial;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.telephony.SmsMessage;
 
 /***
  * 通过短信读取短信内容
  */
-public class MessageReceiver extends BroadcastReceiver {
+public class MessageReceiverFromSms extends BroadcastReceiver {
 
-    public MessageReceiver() {
+    public MessageReceiverFromSms() {
         super();
     }
 
@@ -24,7 +25,11 @@ public class MessageReceiver extends BroadcastReceiver {
             SmsMessage[] message = new SmsMessage[pdus.length];
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < message.length; i++) {
-                message[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                if (Build.VERSION.SDK_INT == 23) {
+                    message[i] = SmsMessage.createFromPdu((byte[]) pdus[i], intent.getStringExtra("format"));
+                } else {
+                    message[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
                 sb.append(message[i].getDisplayMessageBody());
             }
             final String password = UtilsHelpers.getPassword(sb.toString());
